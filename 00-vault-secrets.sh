932cebe8020be793b7e5bc0c4b6fdba0a6a9cd98
sheksum <48729050+sheksum@users.noreply.github.com>
2026-04-23 12:51:11 -0400

    Update 00-vault-secrets.sh

--- 00-vault-secrets.sh
+++ 00-vault-secrets.sh
@@ -1,3 +1,8 @@
-# On cpeg
-sudo grep "Can't locate CSN" /var/log/dirsrv/slapd-IPA-CALIX-LOCAL/errors | head -3
-sudo ls -la /var/log/dirsrv/slapd-IPA-CALIX-LOCAL/errors*
+# On cpeg - verify pln-petipareplica is cleaned out
+kinit admin
+ipa-replica-manage list
+ipa host-show pln-petipareplica.ipa.calix.local 2>&1
+
+# Check for leftover RUV
+sudo dsconf -D "cn=Directory Manager" ldap://cpeg-ipareplica.ipa.calix.local \
+  repl-agmt list --suffix="dc=ipa,dc=calix,dc=local" 2>&1 | grep -iE "petipareplica|nsds50ruv"
