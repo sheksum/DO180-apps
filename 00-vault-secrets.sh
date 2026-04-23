365a1a2d727366f665964e9cf537c5a0b0994a99
sheksum <48729050+sheksum@users.noreply.github.com>
2026-04-23 18:49:24 -0400

    Update 00-vault-secrets.sh

--- 00-vault-secrets.sh
+++ 00-vault-secrets.sh
@@ -1,11 +1,40 @@
-cat <<EOF >/etc/apt/preferences.d/firefox
+root@sdaxos-u22-mrollins:~# cat <<EOF >/etc/apt/preferences.d/firefox
 Package: firefox*
 Pin: origin pln-landscape01.caal.dev
 Pin-Priority: 1001
 EOF
-
-
-apt update
-apt-cache policy firefox
-
-apt-get install -s firefox
+root@sdaxos-u22-mrollins:~#
+root@sdaxos-u22-mrollins:~#
+root@sdaxos-u22-mrollins:~# apt update
+Hit:1 http://pln-landscape01.caal.dev/repository/standalone/ubuntu-jammy-onprem jammy-updates InRelease
+Hit:2 http://pln-landscape01.caal.dev/repository/standalone/ubuntu-jammy-onprem jammy-security InRelease
+Hit:3 http://pln-landscape01.caal.dev/repository/standalone/ubuntu-jammy-onprem jammy InRelease
+Hit:4 http://pln-landscape01.caal.dev/repository/standalone/third-party stable-grafana-onprem InRelease
+Hit:5 http://pln-landscape01.caal.dev/repository/standalone/third-party stable-google-chrome-onprem InRelease
+Hit:6 http://pln-landscape01.caal.dev/repository/standalone/third-party stable-microsoft-vscode-onprem InRelease
+Hit:7 http://pln-landscape01.caal.dev/repository/standalone/third-party stable-mozilla-firefox-onprem InRelease
+Hit:8 http://pln-landscape01.caal.dev/repository/standalone/ubuntu-jammy-onprem jammy-docker-jammy-onprem InRelease
+Hit:9 https://packages.microsoft.com/repos/code stable InRelease
+Reading package lists... Done
+Building dependency tree... Done
+Reading state information... Done
+2 packages can be upgraded. Run 'apt list --upgradable' to see them.
+root@sdaxos-u22-mrollins:~# apt-cache policy firefox
+firefox:
+  Installed: (none)
+  Candidate: 1:1snap1-0ubuntu2
+  Version table:
+     1:1snap1-0ubuntu2 1001
+        500 http://pln-landscape01.caal.dev/repository/standalone/ubuntu-jammy-onprem jammy/main amd64 Packages
+     150.0~build1 1001
+        500 http://pln-landscape01.caal.dev/repository/standalone/third-party stable-mozilla-firefox-onprem/main amd64 Packages
+root@sdaxos-u22-mrollins:~# apt-get install -s firefox
+Reading package lists... Done
+Building dependency tree... Done
+Reading state information... Done
+The following NEW packages will be installed:
+  firefox
+0 upgraded, 1 newly installed, 0 to remove and 2 not upgraded.
+Inst firefox (1:1snap1-0ubuntu2 jammy [amd64])
+Conf firefox (1:1snap1-0ubuntu2 jammy [amd64])
+root@sdaxos-u22-mrollins:~#
