98a8e37f1c84e7dffd869e6f3eb828f578bb9f05
sheksum <48729050+sheksum@users.noreply.github.com>
2026-04-23 17:53:09 -0400

    Update 00-vault-secrets.sh

--- 00-vault-secrets.sh
+++ 00-vault-secrets.sh
@@ -1,1 +1,9 @@
-deb [arch=amd64] http://pln-landscape01.caal.dev/repository/standalone/third-party mozilla main
+Ign:8 http://pln-landscape01.caal.dev/repository/standalone/third-party mozilla InRelease
+Err:9 http://pln-landscape01.caal.dev/repository/standalone/third-party mozilla Release
+  404  Not Found [IP: 10.172.249.104 80]
+Hit:10 https://packages.microsoft.com/repos/code stable InRelease
+Reading package lists... Done
+E: The repository 'http://pln-landscape01.caal.dev/repository/standalone/third-party mozilla Release' does not have a Release file.
+N: Updating from such a repository can't be done securely, and is therefore disabled by default.
+N: See apt-secure(8) manpage for repository creation and user configuration details.
+root@sdaxos-u22-mrollins:~#
