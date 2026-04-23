a0adb7dd9eb7201f9b7380295683c7e2a99a7cb5
sheksum <48729050+sheksum@users.noreply.github.com>
2026-04-23 17:41:12 -0400

    Update 00-vault-secrets.sh

--- 00-vault-secrets.sh
+++ 00-vault-secrets.sh
@@ -1,1 +1,8 @@
-curl -fsSL https://packages.mozilla.org/apt/repo-signing-key.gpg | gpg --dearmor | gpg --armor
+b"aptmethod error receiving 'https://packages.mozilla.org/apt/dists/stable-firefox-onprem/InRelease':
+'404  Not Found [IP: 34.160.78.70 443]'
+aptmethod error receiving 'https://packages.mozilla.org/apt/dists/stable-firefox-onprem/Release':
+'404  Not Found [IP: 34.160.78.70 443]'
+aptmethod error receiving 'https://packages.mozilla.org/apt/dists/stable-firefox-onprem/Release.gpg':
+'404  Not Found [IP: 34.160.78.70 443]'
+There have been errors!
+"
