5bff46ad53fe699e06078f10f1d91e7b3f132d98
sheksum <48729050+sheksum@users.noreply.github.com>
2026-04-23 18:02:37 -0400

    Update 00-vault-secrets.sh

--- 00-vault-secrets.sh
+++ 00-vault-secrets.sh
@@ -1,1 +1,20 @@
-ls /var/lib/landscape/landscape-repository/standalone/
+root@pln-landscape01:~# lsctl restart
+dpkg-query: no path found matching pattern /sbin/init
+== landscape-appserver ==
+== landscape-async-frontend ==
+== landscape-job-handler ==
+== landscape-msgserver ==
+== landscape-pingserver ==
+== landscape-api ==
+== landscape-package-upload ==
+== landscape-package-search ==
+== landscape-hostagent-messenger ==
+== landscape-hostagent-consumer ==
+== landscape-secrets-service ==
+root@pln-landscape01:~# ls /var/lib/landscape/landscape-repository/standalone/
+third-party  ubuntu-bionic-onprem  ubuntu-focal-onprem  ubuntu-jammy-onprem  ubuntu-noble-onprem  ubuntu-xenial-onprem
+root@pln-landscape01:~# ls /var/lib/landscape/landscape-repository/standalone/third-party/
+conf  db  dists  incoming  lists  locks  pool  temp
+root@pln-landscape01:~# ls /var/lib/landscape/landscape-repository/standalone/third-party/dists
+stable-firefox-onprem  stable-google-chrome-onprem  stable-grafana-onprem  stable-microsoft-vscode-onprem
+root@pln-landscape01:~#
