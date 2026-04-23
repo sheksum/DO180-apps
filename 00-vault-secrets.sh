060a5d3dd8e1db3184d7389c2540beea520f93e7
sheksum <48729050+sheksum@users.noreply.github.com>
2026-04-23 12:56:16 -0400

    Update 00-vault-secrets.sh

--- 00-vault-secrets.sh
+++ 00-vault-secrets.sh
@@ -1,30 +1,14 @@
-# On cpeg
-kinit admin
-ipa host-del pln-petipareplica.ipa.calix.local --updatedns 2>&1
-
-# Verify
-ipa host-show pln-petipareplica.ipa.calix.local 2>&1
-
-
-# On cpeg
-ipa dnsrecord-add ipa.calix.local pln-petipareplica --a-rec=10.172.248.57
-dig @172.23.49.10 pln-petipareplica.ipa.calix.local +short
-
-# On cpeg - pre-create host (generates one-time password)
-ipa host-add pln-petipareplica.ipa.calix.local --ip-address=10.172.248.57 --force
-
-# Add to ipaservers group (required for replica)
-ipa hostgroup-add-member ipaservers --hosts=pln-petipareplica.ipa.calix.local
-
-# Pre-create HTTP service
-ipa service-add HTTP/pln-petipareplica.ipa.calix.local
-
-# Verify all three
-ipa host-show pln-petipareplica.ipa.calix.local
-ipa service-show HTTP/pln-petipareplica.ipa.calix.local
-
-[root@cpeg-ipareplica ~]# ipa host-add pln-petipareplica.ipa.calix.local --ip-address=10.172.248.57 --force
-ipa: ERROR: IP address 10.172.248.57 is already assigned in domain ipa.calix.local..
-[root@cpeg-ipareplica ~]# ipa service-add HTTP/pln-petipareplica.ipa.calix.local
-ipa: ERROR: The host 'pln-petipareplica.ipa.calix.local' does not exist to add a service to.
+[root@cpeg-ipareplica ~]#
+[root@cpeg-ipareplica ~]# ipa host-show pln-petipareplica.ipa.calix.local
+  Host name: pln-petipareplica.ipa.calix.local
+  Principal name: host/pln-petipareplica.ipa.calix.local@IPA.CALIX.LOCAL
+  Principal alias: host/pln-petipareplica.ipa.calix.local@IPA.CALIX.LOCAL
+  Password: False
+  Keytab: False
+  Managed by: pln-petipareplica.ipa.calix.local
+[root@cpeg-ipareplica ~]# ipa service-show HTTP/pln-petipareplica.ipa.calix.local
+  Principal name: HTTP/pln-petipareplica.ipa.calix.local@IPA.CALIX.LOCAL
+  Principal alias: HTTP/pln-petipareplica.ipa.calix.local@IPA.CALIX.LOCAL
+  Keytab: False
+  Managed by: pln-petipareplica.ipa.calix.local
 [root@cpeg-ipareplica ~]#
