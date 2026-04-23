344480104ce66bb7d067d6805d0d02b342220a8c
sheksum <48729050+sheksum@users.noreply.github.com>
2026-04-23 12:47:57 -0400

    Update 00-vault-secrets.sh

--- 00-vault-secrets.sh
+++ 00-vault-secrets.sh
@@ -1,66 +1,22 @@
-# On pln-petipareplica
-sudo ipa-server-install --uninstall -U
+[root@pln-petipareplica ~]# ipa-server-install --uninstall -U
+WARNING:
+IPA server is not configured on this system. If you want to install the
+IPA server, please install it using 'ipa-server-install'.
+Server removal aborted:
 
-# Remove any stale keytabs / IPA config  
-sudo rm -f /etc/krb5.keytab
-sudo rm -f /var/lib/ipa/gssproxy/http.keytab
-sudo rm -rf /var/lib/ipa/sysrestore/
-sudo rm -rf /etc/dirsrv/slapd-IPA-CALIX-LOCAL
-
-
-# On cpeg
-kinit admin
-
-# Remove host entry (which removes associated services/keytabs)
-ipa host-del pln-petipareplica.ipa.calix.local --updatedns 2>&1
-
-# Verify it's gone
-ipa host-show pln-petipareplica.ipa.calix.local 2>&1
-ipa-replica-manage list
-
-# Check DNS is also clean
-dig @172.23.49.10 pln-petipareplica.ipa.calix.local +short
-
-
-sudo sed -i 's|^ipa_server = cpeg-ipareplica.ipa.calix.local|ipa_server = cpeg-ipareplica.ipa.calix.local, pln-petipareplica.ipa.calix.local|' /etc/sssd/sssd.conf
-sudo grep ipa_server /etc/sssd/sssd.conf
-sudo sss_cache -E
-sudo systemctl restart sssd
-
-# Run for 30 min to confirm improvement
-for i in $(seq 1 60); do
-  echo "$(date +%H:%M:%S) $(sudo sssctl domain-status ipa.calix.local | grep 'Online status')"
-  sleep 30
-done
-
-
-# On cpeg
-sudo dsconf -D "cn=Directory Manager" ldap://cpeg-ipareplica.ipa.calix.local \
-  repl-agmt list --suffix="dc=ipa,dc=calix,dc=local" 2>&1 | \
-  grep -E "^dn:|nsds5replicaHost|nsds5ReplicaEnabled|nsds5replicaLastUpdateStatus:"
-
-
-  # On cpeg
-kinit admin
-ipa user-add testrepl --first=Test --last=Replication
-
-# Immediately query from pln-petipareplica
-ldapsearch -x -H ldap://pln-petipareplica.ipa.calix.local \
-  -b "cn=users,cn=accounts,dc=ipa,dc=calix,dc=local" "(uid=testrepl)" uid 2>&1 | tail -5
-
-# Should return the entry. If not, wait 30s and retry.
-
-# Clean up
-ipa user-del testrepl
-
-[root@cpeg-ipareplica ~]# ipa user-add testrepl --first=Test --last=Replication
-ipa: ERROR: user with name "testrepl" already exists
-[root@cpeg-ipareplica ~]# ipa user-add testrepls --first=Test --last=Replication
-----------------------
-Added user "testrepls"
-----------------------
-  User login: testrepls
-  First name: Test
+Replication topology in suffix 'domain' is disconnected:
+Topology does not allow server cpeg-ipareplica.ipa.calix.local to replicate with servers:
+    pln-petipareplica.ipa.calix.local
+Topology does not allow server nan-ipareplica01.ipa.calix.local to replicate with servers:
+    pln-petipareplica.ipa.calix.local
+Topology does not allow server ric-ipareplica.ipa.calix.local to replicate with servers:
+    pln-petipareplica.ipa.calix.local
+Topology does not allow server ric-ipareplica01.ipa.calix.local to replicate with servers:
+    pln-petipareplica.ipa.calix.local
+Topology does not allow server sjc-ipareplica01.ipa.calix.local to replicate with servers:
+    pln-petipareplica.ipa.calix.local.
+The ipa-server-install command failed. See /var/log/ipaserver-uninstall.log for more information
+[root@pln-petipareplica ~]#  First name: Test
   Last name: Replication
   Full name: Test Replication
   Display name: Test Replication
