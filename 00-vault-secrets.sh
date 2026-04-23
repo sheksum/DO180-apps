f17647667d4454dcdd0e37e3ffce690ffd9ba2f4
sheksum <48729050+sheksum@users.noreply.github.com>
2026-04-23 17:32:05 -0400

    Update 00-vault-secrets.sh

--- 00-vault-secrets.sh
+++ 00-vault-secrets.sh
@@ -1,7 +1,1 @@
-Thanks Haji – I’ll start reviewing with Vation and pull you in when needed.
-
-FYI – Signed up for some consulting from Gitlab and starting the project within the next two weeks at most, would love to have you both for the first planning call and introduce Glen who will be running the project.  We’ve got some security , runner and image requirements for containers in the cluster so the runners have tools to work with. 
-What I’m hoping is you all can help with the runner/image/worker deployments in the project.
-
-Thanks
-Jim
+curl -fsSL https://packages.mozilla.org/apt/repo-signing-key.gpg | gpg --dearmor | gpg --armor
