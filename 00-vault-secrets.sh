ca9c5fc02d3d8d1e729f3d021a9e51bfbd41d6b4
sheksum <48729050+sheksum@users.noreply.github.com>
2026-04-23 12:11:43 -0400

    Update 00-vault-secrets.sh

--- 00-vault-secrets.sh
+++ 00-vault-secrets.sh
@@ -1,29 +1,10 @@
-[root@pln-petipareplica ~]# dig @172.23.49.10 pln-petipareplica.ipa.calix.local +short
-10.172.248.57
-[root@pln-petipareplica ~]#
-[root@pln-petipareplica ~]# nc -zv cpeg-ipareplica.ipa.calix.local 389
-Ncat: Version 7.92 ( https://nmap.org/ncat )
-Ncat: Connected to 172.23.49.10:389.
-Ncat: 0 bytes sent, 0 bytes received in 0.06 seconds.
-[root@pln-petipareplica ~]# nc -zv cpeg-ipareplica.ipa.calix.local 88
-Ncat: Version 7.92 ( https://nmap.org/ncat )
-Ncat: Connected to 172.23.49.10:88.
-Ncat: 0 bytes sent, 0 bytes received in 0.06 seconds.
-[root@pln-petipareplica ~]# nc -zv cpeg-ipareplica.ipa.calix.local 464
-Ncat: Version 7.92 ( https://nmap.org/ncat )
-Ncat: Connected to 172.23.49.10:464.
-Ncat: 0 bytes sent, 0 bytes received in 0.06 seconds.
-[root@pln-petipareplica ~]# grep -i "pln\|cpeg" /etc/hosts
-10.172.248.57 pln-petipareplica.ipa.calix.local pln-petipareplica
-172.23.49.10	cpeg-ipareplica.ipa.calix.local
-[root@pln-petipareplica ~]# date
-Thu Apr 23 16:10:38 UTC 2026
-[root@pln-petipareplica ~]# timedatectl status | grep -i "NTP\|sync"
-System clock synchronized: yes
-              NTP service: active
-[root@pln-petipareplica ~]#
-
-[root@cpeg-ipareplica ~]# dig @172.23.49.10 pln-petipareplica.ipa.calix.local +short
-10.172.248.57
-[root@cpeg-ipareplica ~]#
-[root@cpeg-ipareplica ~]#
+# On pln-petipareplica
+ipa-replica-install \
+  --server=cpeg-ipareplica.ipa.calix.local \
+  --domain=ipa.calix.local \
+  --realm=IPA.CALIX.LOCAL \
+  --principal=admin \
+  --admin-password='vV+DyEsStenbkQq5TxLP5ly2wq/X4CPXW38mpDpCHUU=' \
+  --no-ntp \
+  --skip-conncheck \
+  --unattended 2>&1 | tee /var/log/pln-petipareplica-install-$(date +%Y%m%d-%H%M).log
