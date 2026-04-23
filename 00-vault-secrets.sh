887db1b5729a6c66e45cd945429df7a31ba05aff
sheksum <48729050+sheksum@users.noreply.github.com>
2026-04-23 15:59:20 -0400

    Update 00-vault-secrets.sh

--- 00-vault-secrets.sh
+++ 00-vault-secrets.sh
@@ -1,22 +1,8 @@
-[root@aamslc4kbc01 ~]# 
-[root@aamslc4kbc01 ~]# 
-[root@aamslc4kbc01 ~]# 
-[root@aamslc4kbc01 ~]# kubectl -n pgweb get ingress kindo-pgweb -o yaml | grep -A5 -E 'tls:|host:'
-  - host: pgweb.kbai.ttmtech.com
-    http:
-      paths:
-      - backend:
-          service:
-            name: kindo-pgweb
-[root@aamslc4kbc01 ~]# 
-[root@aamslc4kbc01 ~]# 
-[root@aamslc4kbc01 ~]# kubectl -n pgweb get ingress kindo-pgweb -o jsonpath='{.spec.tls}' ; echo
+# Show the full redirect chain with headers
+curl -skI http://pgweb.kbai.ttmtech.com/
 
-[root@aamslc4kbc01 ~]# kubectl -n pgweb get ingress kindo-pgweb -o jsonpath='{range .spec.rules[*]}host={.host} paths={.http.paths[*].path}{"\n"}{end}'
-host=pgweb.kbai.ttmtech.com paths=/
-[root@aamslc4kbc01 ~]# 
-[root@aamslc4kbc01 ~]# curl -sk -o /dev/null -w "HTTP %{http_code} | redirected-to: %{redirect_url}\n" http://pgweb.kbai.ttmtech.com/
-HTTP 200 | redirected-to: 
-[root@aamslc4kbc01 ~]# curl -sk -o /dev/null -w "HTTPS %{http_code}\n" https://pgweb.kbai.ttmtech.com/
-HTTPS 200
-[root@aamslc4kbc01 ~]# 
+# Or verbose to see everything
+curl -skv http://pgweb.kbai.ttmtech.com/ 2>&1 | grep -E '^< HTTP|^< Location|^> GET' | head -10
+
+# And follow redirects to see the chain
+curl -skIL http://pgweb.kbai.ttmtech.com/ 2>&1 | grep -E '^HTTP|^Location' | head
