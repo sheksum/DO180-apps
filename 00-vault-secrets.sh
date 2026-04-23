d5c9502a7bf62eda96962cbd5ca290779c8857a3
sheksum <48729050+sheksum@users.noreply.github.com>
2026-04-23 18:01:52 -0400

    Update 00-vault-secrets.sh

--- 00-vault-secrets.sh
+++ 00-vault-secrets.sh
@@ -1,12 +1,1 @@
-root@sdaxos-u22-mrollins:~#
-root@sdaxos-u22-mrollins:~# curl http://pln-landscape01.caal.dev/repository/standalone/third-party/dists/mozilla/Release
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>404 Not Found</title>
-</head><body>
-<h1>Not Found</h1>
-<p>The requested URL was not found on this server.</p>
-<hr>
-<address>Apache/2.4.58 (Ubuntu) Server at pln-landscape01.caal.dev Port 80</address>
-</body></html>
-root@sdaxos-u22-mrollins:~#
+ls /var/lib/landscape/landscape-repository/standalone/
