ae9913eaac72fbaa013ad24a7dc3ea8b42135870
sheksum <48729050+sheksum@users.noreply.github.com>
2026-04-23 18:47:28 -0400

    Update 00-vault-secrets.sh

--- 00-vault-secrets.sh
+++ 00-vault-secrets.sh
@@ -1,25 +1,10 @@
-root@sdaxos-u22-mrollins:~# apt-get update
-Hit:1 http://pln-landscape01.caal.dev/repository/standalone/ubuntu-jammy-onprem jammy-updates InRelease
-Hit:2 http://pln-landscape01.caal.dev/repository/standalone/ubuntu-jammy-onprem jammy-security InRelease
-Hit:3 http://pln-landscape01.caal.dev/repository/standalone/ubuntu-jammy-onprem jammy InRelease
-Hit:4 http://pln-landscape01.caal.dev/repository/standalone/third-party stable-grafana-onprem InRelease
-Hit:5 http://pln-landscape01.caal.dev/repository/standalone/third-party stable-google-chrome-onprem InRelease
-Hit:6 http://pln-landscape01.caal.dev/repository/standalone/third-party stable-microsoft-vscode-onprem InRelease
-Get:7 http://pln-landscape01.caal.dev/repository/standalone/third-party stable-mozilla-firefox-onprem InRelease [2,263 B]
-Hit:8 http://pln-landscape01.caal.dev/repository/standalone/ubuntu-jammy-onprem jammy-docker-jammy-onprem InRelease
-Hit:9 https://packages.microsoft.com/repos/code stable InRelease
-Get:10 http://pln-landscape01.caal.dev/repository/standalone/third-party stable-mozilla-firefox-onprem/main amd64 Packages [2,068 B]
-Fetched 4,331 B in 9s (495 B/s)
-Reading package lists... Done
-root@sdaxos-u22-mrollins:~# apt-get update
-Hit:1 http://pln-landscape01.caal.dev/repository/standalone/ubuntu-jammy-onprem jammy-updates InRelease
-Hit:2 http://pln-landscape01.caal.dev/repository/standalone/ubuntu-jammy-onprem jammy-security InRelease
-Hit:3 http://pln-landscape01.caal.dev/repository/standalone/ubuntu-jammy-onprem jammy InRelease
-Hit:4 http://pln-landscape01.caal.dev/repository/standalone/third-party stable-grafana-onprem InRelease
-Hit:5 http://pln-landscape01.caal.dev/repository/standalone/third-party stable-google-chrome-onprem InRelease
-Hit:6 http://pln-landscape01.caal.dev/repository/standalone/third-party stable-microsoft-vscode-onprem InRelease
-Hit:7 http://pln-landscape01.caal.dev/repository/standalone/third-party stable-mozilla-firefox-onprem InRelease
-Hit:8 http://pln-landscape01.caal.dev/repository/standalone/ubuntu-jammy-onprem jammy-docker-jammy-onprem InRelease
-Hit:9 https://packages.microsoft.com/repos/code stable InRelease
-Reading package lists... Done
+root@sdaxos-u22-mrollins:~# apt-cache policy firefox
+firefox:
+  Installed: (none)
+  Candidate: 1:1snap1-0ubuntu2
+  Version table:
+     1:1snap1-0ubuntu2 500
+        500 http://pln-landscape01.caal.dev/repository/standalone/ubuntu-jammy-onprem jammy/main amd64 Packages
+     150.0~build1 500
+        500 http://pln-landscape01.caal.dev/repository/standalone/third-party stable-mozilla-firefox-onprem/main amd64 Packages
 root@sdaxos-u22-mrollins:~#
