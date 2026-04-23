cf9086f54c717115e2967f6cf81f7ab621c096ab
sheksum <48729050+sheksum@users.noreply.github.com>
2026-04-23 12:32:30 -0400

    Update 00-vault-secrets.sh

--- 00-vault-secrets.sh
+++ 00-vault-secrets.sh
@@ -1,26 +1,22 @@
-[root@pln-petipareplica ~]#
-[root@pln-petipareplica ~]# ldapsearch -x -H ldap://pln-petipareplica.ipa.calix.local -b "cn=users,cn=accounts,dc=ipa,dc=calix,dc=local" "(uid=hsuma)" uid uidNumber 2>&1 | tail -10
-dn: uid=hsuma,cn=users,cn=accounts,dc=ipa,dc=calix,dc=local
-uid: hsuma
-uidNumber: 13232
+# On pln-petipareplica
+sudo ipa-server-install --uninstall -U
 
-# search result
-search: 2
-result: 0 Success
+# Remove any stale keytabs / IPA config  
+sudo rm -f /etc/krb5.keytab
+sudo rm -f /var/lib/ipa/gssproxy/http.keytab
+sudo rm -rf /var/lib/ipa/sysrestore/
+sudo rm -rf /etc/dirsrv/slapd-IPA-CALIX-LOCAL
 
-# numResponses: 2
-# numEntries: 1
-[root@pln-petipareplica ~]#
-[root@pln-petipareplica ~]#
-[root@pln-petipareplica ~]# sudo ss -tlnp | grep -E ":88|:464|:389"
-LISTEN 0      5                 0.0.0.0:464        0.0.0.0:*    users:(("kadmind",pid=192587,fd=9))
-LISTEN 0      5                 0.0.0.0:88         0.0.0.0:*    users:(("krb5kdc",pid=192520,fd=9),("krb5kdc",pid=192519,fd=9),("krb5kdc",pid=192518,fd=9),("krb5kdc",pid=192517,fd=9))
-LISTEN 0      5                    [::]:464           [::]:*    users:(("kadmind",pid=192587,fd=10))
-LISTEN 0      5                    [::]:88            [::]:*    users:(("krb5kdc",pid=192520,fd=10),("krb5kdc",pid=192519,fd=10),("krb5kdc",pid=192518,fd=10),("krb5kdc",pid=192517,fd=10))
-LISTEN 0      128                     *:389              *:*    users:(("ns-slapd",pid=192825,fd=7))
-[root@pln-petipareplica ~]#
-[root@pln-petipareplica ~]# sudo systemctl is-active dirsrv@IPA-CALIX-LOCAL krb5kdc kadmin
-active
-active
-active
-[root@pln-petipareplica ~]#
+
+# On cpeg
+kinit admin
+
+# Remove host entry (which removes associated services/keytabs)
+ipa host-del pln-petipareplica.ipa.calix.local --updatedns 2>&1
+
+# Verify it's gone
+ipa host-show pln-petipareplica.ipa.calix.local 2>&1
+ipa-replica-manage list
+
+# Check DNS is also clean
+dig @172.23.49.10 pln-petipareplica.ipa.calix.local +short
